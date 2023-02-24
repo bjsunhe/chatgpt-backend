64a61f59a8731c2091db4593d7582bcc14438339
SUN He (ATMO-3CN/PJ-CDT) <snh3sgh@bosch.com>
2023-02-24 12:47:53 +0800

    api

--- controller/article.js
+++ controller/article.js
@@ -58,27 +58,40 @@ const addArticle=async (req,res,next)=>{
                 presence_penalty: 0.0,
                 'max_tokens':2048
             });
-        }catch(e){
-            console.log(e)
-        }
+
+            const article=new Article({
+                title,
+                prompt,
+                source,
+                content,
+                result:completion.data.choices[0].text,
+                author
+            })
+            await article.save()
+            res.status(200).json(
+                {
+                    article
+                }
+            )
+        }catch(error) {
+            // Consider adjusting the error handling logic for your use case
+            if (error.response) {
+              console.error(error.response.status, error.response.data);
+              res.status(error.response.status).json(error.response.data);
+            } else {
+              console.error(`Error with OpenAI API request: ${error.message}`);
+              res.status(500).json({
+                error: {
+                  message: 'An error occurred during your request.',
+                }
+              });
+            }
+          }
 
 
         
 
-        const article=new Article({
-            title,
-            prompt,
-            source,
-            content,
-            result:completion.data.choices[0].text,
-            author
-        })
-        await article.save()
-        res.status(200).json(
-            {
-                article
-            }
-        )
+        
     }catch(error){
         next(error)
     }
