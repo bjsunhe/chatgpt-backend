1f3d687932d58de66cdb364da96537318ac25d15
SUN He (ATMO-3CN/PJ-CDT) <snh3sgh@bosch.com>
2023-02-24 12:03:50 +0800

    api

--- chatgpt-api/index.js
+++ chatgpt-api/index.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: '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',
+  apiKey: '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',
 });
 const openai = new OpenAIApi(configuration);
 
